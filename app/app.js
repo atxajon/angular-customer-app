294b778580c9332dd7c943080cadeddbc8ea3d4e
Jon Atxa <atxajon@gmail.com>
2015-07-01 08:37:11 +0100

    configured routing to marry view tpl with controller; refactored code to do it

--- app/app.js
+++ app/app.js
@@ -2,4 +2,13 @@
 
   var app = angular.module('customersApp', ['ngRoute']);
 
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'CustomersController',
+        templateUrl: 'app/views/customers.html',
+      })
+      .otherwise({redirectTo: '/'});
+  });
+
 }());
