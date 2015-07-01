44756503ad5b12f96df8b29fd7ed4fa582076403
Jon Atxa <atxajon@gmail.com>
2015-07-01 18:45:27 +0100

    added a second route for customer orders, with its view

--- app/app.js
+++ app/app.js
@@ -8,6 +8,10 @@
         controller: 'CustomersController',
         templateUrl: 'app/views/customers.html',
       })
+      .when('/orders/:customerId', {
+        controller: 'OrdersController',
+        templateUrl: 'app/views/orders.html',
+      })
       .otherwise({redirectTo: '/'});
   });
 
